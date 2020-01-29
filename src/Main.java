a7378660c53cfc41b4d571fc81340d72df76fe01
Twan <twanhog@gmail.com>
2020-01-29 13:59:44 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello class A!");
+        System.out.println("Kunnen jullie het bijhouden!?!?");
+    }
+
+}
